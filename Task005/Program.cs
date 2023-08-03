73bb995f41492defeaecc58d071f8f0159542305
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-08-03 19:29:20 +0300

    add Task 005 Exponentiation

---
+++ Task005/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+//Метод, который запрашивает число:
+
+int PutNumber(string msg)
+{
+    Console.Write(msg);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+
+//Метод возведения в степень:
+
+int Exponentiation(int A, int B)
+{
+    int result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+//Сама программа:
+
+int x = PutNumber("Введите число X: ");
+int y = PutNumber("Введите степень Y, в которую надо возвести число: ");
+
+Console.WriteLine($"X в степени Y равно {Exponentiation(x, y)}");
