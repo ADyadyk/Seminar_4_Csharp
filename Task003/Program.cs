d717abe8d29881a05e170087ad7a2e11c0fc9197
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-08-03 19:01:10 +0300

    add classwork

---
+++ Task003/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 2: Определить, является ли заданное шестизначное число счастливым. 
+(Счастливым называют такое шестизначное число, что сумма его первых трех цифр равна сумме его 
+последних трех цифр, порядок чисел в сумме не важен). Решить самым оптимальным способом.
+*/
+
+//Метод, который запрашивает шестизначное число
+string PutNumber()
+{
+    Console.Write("Введите шестизначное число: ");
+    string result = Console.ReadLine();
+    return result;
+}
+
+//Проверка числа
+bool Check(string num)
+{
+    if (num.Length == 6) return true;
+    else return false;
+}
+
+
+//Happy method
+
+void FindHappy(string num)
+{
+
+    if (num[0]+num[1]+num[2] == num[3]+num[4]+num[5])
+    {
+        Console.WriteLine("Число счастливое!");
+    }
+    else 
+    {
+        Console.WriteLine("Число не счастливое!");
+    }
+}
+
+//Программа
+
+string number = PutNumber();
+
+if (Check(number)) FindHappy(number);
+else Console.WriteLine("Число не шестизначное!");
---
+++ Task004/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+
+[1,0,1,1,0,1,0,0]
+*/
+
+//Метод рандом
+
+int[] GiveMas(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i=0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(2);
+    }
+    return arr;
+}
+
+Console.Write("Введите размер массива: ");
+int len = int.Parse(Console.ReadLine()!);
+
+int[] array = GiveMas(len);
+
+Console.WriteLine($"{String.Join("  ", array)}");
+
+
+
+
+
+
+
+
+
+/*
+// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+int[] array = GetRange(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+//method
+int[] GetRange(int size){
+    int[] massive = new int[size];
+    for(int i = 0; i < massive.Length; i++){
+        massive[i] = new Random().Next(2);
+    }
+    return massive;
+}
+*/
